3aabe1590e4e477e39059dd1f00ac14280f6835b
Deepak Sharma <deepaksharma@deepaks-mbp-2.mynetworksettings.com>
2023-05-31 21:07:03 -0400

    Class code

---
+++ Modules/Class5/Array-Concept-1.js
@@ -0,0 +1,37 @@
+/**
+ * Array:
+ *      which can store one or more valiues at any given time.
+ *      technically, is an object
+ *      represent by []
+ */
+/** 
+ *      Array-Concept-1.js
+ *          length (property)
+ */
+
+let sports = ['FootBall', 'SoCCer', 'GOLf', 'HocKEy', 'MaraTHon', 'RuGBy', 'BasketBaLl']
+console.log(`\nsports -> ${sports}`);
+/**
+ *  sports is a variable which contains an Array of String (String-Array)
+ */
+
+let bills = [12.45, 78.0, 67.12, 900.78];
+console.log(`\nbills -> ${bills}`);
+/**
+ * bills is a variable which contains an Array of Numbers (Number-Array)
+ */
+
+/**
+ * To find the number of values in an Array
+ *      OR
+ * To find the length of Array
+ * 
+ * property: length
+ * return: number
+ */
+
+let sportsLength  = sports.length;
+console.log(`\nsportsLength -> ${sportsLength}`);
+
+let billsLength  = bills.length;
+console.log(`\nbillsLength -> ${billsLength}`);
