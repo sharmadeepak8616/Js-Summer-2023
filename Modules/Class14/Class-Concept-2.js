41ba5ca610d5af2079eaa5d24739bb125d95b91c
Deepak Sharma <deepaksharma@deepaks-mbp-2.mynetworksettings.com>
2023-06-22 09:22:47 -0400

    JS Class code

---
+++ Modules/Class14/Class-Concept-2.js
@@ -0,0 +1,30 @@
+class ClassConcept2 {
+
+    /**
+     * Class is a representation of a generic entity from real-life.
+     * Object of a class represent a particular instance of the class.
+     * 
+     * Syntax to create object of a class:
+     * let objName = new ClassName(); 
+     * 
+     * new ClassName(); --> code to create class object
+     * objName --> variable in which we are storing the class object.
+     * 
+     * 
+     * When we create object of a class
+     *      -> the object gets it's own set of (non-static) global variables with default values; and
+     *          ALL objects shares the static variables.
+     *      -> using the object, we can access the variables and functions present in that class
+     * 
+     * 
+     * 
+     * 
+     */
+
+    /**
+     * this-keyword in programming represents current object using which we are accessing the class.
+     */
+
+
+
+}
